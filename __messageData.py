c26c24fd64d30cabcb1101de5c93d500ecaa3cde
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2022-08-16 00:30:34 +0700

    Update __messageData.py

--- __messageData.py
+++ __messageData.py
@@ -5,102 +5,122 @@ try:
  #from requests_html import HTMLSession
  import json,os
  import requests
+ from requests.sessions import Session
+ from concurrent.futures import ThreadPoolExecutor
+ from threading import Thread,local
+ import __fbTools
 except ImportError:
  pass
 try:
- class api():
-  def sendMessage(threadData,
-                      threadContents,
-                      threadHeaders,
-                      threadID,
-                      setCookies):
-  
-   ajax = requests.Session()
-  
-   postForm = {}
-   postForm["fb_dtsg"] = threadData["fb_dtsg"];
-   postForm["jazoest"] = threadData["jazoest"];
-   postForm["body"] = threadContents;
-   postForm["send"] = "Gửi";
-   postForm["tids"] = "cid.g."+threadID;
-   postForm["csid"] = "LorenBot - with love";
-   
-   threadSeding = ajax.post(
-   "https://m.facebook.com/messages/send/?",
-   headers=threadHeaders,
-   data=postForm
-   ).text;
-   
-  def sendNotification(threadData,
-                        threadContents,
-                        threadHeaders,
-                        threadID,
-                        setCookies):
- 
-   ajax = requests.Session()
-   
-   postForm = {}
-   postForm["fb_dtsg"] = regex('rename="fb_dtsg" value=".*"', threadData)[0].replace('rename="fb_dtsg" value="', '').replace('"','')
-   postForm["jazoest"] = threadData.split('name="jazoest" value="')[1].split('" autocomplete="off" />')[0];
-   postForm["body"] = threadContents;
-   postForm["send"] = "Gửi";
-   postForm["tids"] = "cid.g."+threadTIDBox;
-   postForm["csid"] = threadData.split('name="csid" value="')[1].split('" /></form>')[0];
-   
-   threadSeding = ajax.post(
-   "https://m.facebook.com/messages/send/?",
-   headers=threadHeaders,
-   data=postForm
-   ).text;
-   
-   
-  def sendAdmin(threadData,
-               threadContents,
-               threadHeaders,
-               setCookies):
-  
-   ajax = requests.Session();
-   idUser = setCookies.split("user=")[1].split(";")[0];
+  thread_local = local()
+  def getSessionRequests() -> Session:
+      if not hasattr(thread_local,'session'):
+          thread_local.session = requests.Session()
+      return thread_local.session
 
-   for adminID in json.loads(open(os.path.dirname("/storage/emulated/0/Download/LorenBot/") + "/configMain.json","r").read())["LORENBOT"]["adminList"]:
-    postForm = {}
-    postForm["tids"] = "cid.c."+adminID+":"+idUser;
-    postForm["ids["+adminID+"]"] = adminID;
-    postForm["body"] = threadContents;
-    postForm["fb_dtsg"] = threadData["fb_dtsg"];
-    postForm["jazoes"] = threadData["jazoest"];
-    postForm["__user"] = idUser;
-
-    threadSeding = ajax.post(
-    'https://m.facebook.com/messages/send/?',
-    headers=threadHeaders,
-    data=postForm
-    )
+  def POST(url:str, headers, data):
+      session = getSessionRequests()
+      with session.post(url, headers=headers, data=data) as response:
+          return {
+            "contentsWebsite": response.text,
+            "statusCode": response.status_code,
+            "urlLocate": response.url,
+            "timeRequests": response.elapsed.total_seconds()
+          }
+  def replyRequests(url, headers, data) -> None:
+      with ThreadPoolExecutor(max_workers=10) as executor:
+          var = POST(url, headers=headers, data=data)
+          return var
+  class api():
+    def sendMessage(threadData:str,
+                    threadContents:str,
+                    threadHeaders:str,
+                    threadID:str):
     
-  def sendUser(threadData,
-                 threadContents,
-                 threadHeaders,
-                 UserID,
-                 setCookies):
-   
-   ajax = requests.Session()
-   idUser = setCookies.split("user=")[1].split(";")[0];
-   postForm = {}
-   postForm["tids"] = "cid.c."+UserID+":"+idUser;
-   postForm["ids["+UserID+"]"] = UserID;
-   postForm["body"] = threadContents;
-   postForm["fb_dtsg"] = threadData["fb_dtsg"];
-   postForm["jazoes"] = threadData["jazoest"];
-   postForm["__user"] = idUser;
+    # ajax = requests.Session()
+    
+      postForm = {}
+      postForm["fb_dtsg"] = threadData["fb_dtsg"];
+      postForm["jazoest"] = threadData["jazoest"];
+      postForm["body"] = threadContents;
+      postForm["send"] = "Gửi";
+      postForm["tids"] = "cid.g."+threadID;
+      postForm["csid"] = "LorenBot - with love (author Project: https://github.com/MinhHuyDev)";
+    
+      sendingPost = replyRequests("https://m.facebook.com/messages/send/?", headers=threadHeaders, data=postForm)
+      
+      try:
+        if (str(sendingPost["urlLocate"]).split("request_type=")[1].split("&")[0].lower() == "send_success" and (sendingPost["statusCode"] == 200)):
+          return True;
+        else:
+          return False;
+      except Exception as errLog:
+        raise SystemExit("\033[1;93mapi.sendMessage cho biết: " + str(errLog))
 
-   threadSeding = ajax.post(
-   'https://m.facebook.com/messages/send/?',
-   headers=threadHeaders,
-   data=postForm
-   )
+    
+    def sendAdmin(threadData:str,
+                  threadContents:str,
+                  threadAdminID:str,
+                  threadID:str,
+                  threadHeaders:str,
+                  setCookies:str):
+    
+      idUser = setCookies.split("user=")[1].split(";")[0];
+      
+      try:
+        for adminID in threadAdminID:
+          postForm = {}
+          postForm["tids"] = "cid.c."+str(adminID)+":"+idUser;
+          postForm["ids["+str(adminID)+"]"] = str(adminID);
+          postForm["body"] = threadContents;
+          postForm["fb_dtsg"] = threadData["fb_dtsg"];
+          postForm["jazoes"] = threadData["jazoest"];
+          postForm["__user"] = idUser;
+  
+          sendingPost = replyRequests("https://m.facebook.com/messages/send/?", headers=threadHeaders, data=postForm)
+          # return sendingPost;
+          try:
+            if (str(sendingPost["urlLocate"]).split("request_type=")[1].split("&")[0].lower() == "send_success" and (sendingPost["statusCode"] == 200)):
+              return True;
+            else:
+              return False;
+          except:
+            raise SystemExit("\033[1;93mapi.sendAdmin cho biết: " + str(errLog))
+      finally: pass
+      # except Exception as errLog:
+      #   sendError = api.sendMessage(threadData, "Không thể gửi tin nhắn đến admin do đã xảy ra lỗi: " + str(errLog), threadHeaders, threadID)
+    def sendUser(threadData:str,
+                  threadContents:str,
+                  threadHeaders:str,
+                  UserID:str,
+                  setCookies:str):
+    
 
+      idUser = setCookies.split("user=")[1].split(";")[0];
+
+      postForm = {}
+      postForm["tids"] = "cid.c."+str(UserID)+":"+str(idUser);
+      postForm["ids["+str(UserID)+"]"] = str(UserID);
+      postForm["body"] = threadContents;
+      postForm["fb_dtsg"] = threadData["fb_dtsg"];
+      postForm["jazoes"] = threadData["jazoest"];
+      postForm["__user"] = idUser;
+
+      sendingPost = replyRequests("https://m.facebook.com/messages/send/?", headers=threadHeaders, data=postForm)
+      try:
+        if (str(sendingPost["urlLocate"]).split("request_type=")[1].split("&")[0].lower() == "send_success" and (sendingPost["statusCode"] == 200)):
+          return True;
+        else:
+          return False;
+      except:
+        raise SystemExit("\033[1;93mapi.sendUser cho biết: " + str(errLog))
 except Exception as errLog:
-  print("Đã xảy ra lỗi: "+ str( errLog ))
+  print("\033[1;93mERROR: \033[1;97m" + str(errLog))
 
-
-# print(api.sendAdmin('zinh bịp',"","",""))
+# setCookies = ''
+# data = __fbTools.dataTools.dataGetHome(setCookies)
+# headers = __fbTools.Headers(setCookies)
+# listad = [4]
+# for i in range(1):
+#   var = api.sendUser(data, "djt me m mark", headers, "4", setCookies)
+#   print(var)
