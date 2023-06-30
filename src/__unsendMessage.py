7947c90af6613786a160ed5030ef6e5cc0f9a32d
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2023-06-30 09:36:14 +0700

    Create __unsendMessage.py

---
+++ src/__unsendMessage.py
@@ -0,0 +1,105 @@
+import requests, json
+import __facebookToolsV2
+
+def digitToChar(digit):
+          if digit < 10:
+               return str(digit)
+          return chr(ord("a") + digit - 10)
+
+def str_base(number, base):
+     if number < 0:
+          return "-" + str_base(-number, base)
+     (d, m) = divmod(number, base)
+     if d > 0:
+          return str_base(d, base) + digitToChar(m)
+     return digitToChar(m)
+
+def parse_cookie_string(cookie_string):
+     cookie_dict = {}
+     cookies = cookie_string.split(";")
+
+     for cookie in cookies:
+          if "=" in cookie:
+               key, value = cookie.split("=")
+          else:
+               pass
+          try: cookie_dict[key] = value
+          except: pass
+
+     return cookie_dict
+
+def Headers(setCookies, dataForm=None):
+     headers = {}
+     headers["Host"] = "www.facebook.com"
+     headers["Connection"] = "keep-alive"
+     if (dataForm != None):
+          headers["Content-Length"] = str(len(dataForm))
+     headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+     headers["Accept"] = "*/*"
+     headers["Origin"] = "https://www.facebook.com"
+     headers["Sec-Fetch-Site"] = "same-origin"
+     headers["Sec-Fetch-Mode"] = "cors"
+     headers["Sec-Fetch-Dest"] = "empty"
+     headers["Referer"] = "https://www.facebook.com/"
+     headers["Accept-Language"] = "vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7"
+
+
+def Main(messageID, dataFB):
+     
+     __reg = attr.ib(0).counter
+     _revision = attr.ib()
+     __reg += 1 
+     dataForm = {}
+     
+     dataForm["fb_dtsg"] = dataFB["fb_dtsg"]
+     dataForm["jazoest"] = dataFB["jazoest"]
+     dataForm["__a"] = 1
+     dataForm["__user"] =str(dataFB["FacebookID"])
+     dataForm["__req"] = str_base(__reg, 36) 
+     dataForm["__rev"] = dataFB["client_revision"]
+     dataForm["av"] = dataFB["FacebookID"]
+     dataForm["message_id"] = messageID
+
+     mainRequests = {
+        "headers": Headers(dataFB["cookieFacebook"], dataForm),
+        "timeout": 5,
+        "url": "https://www.facebook.com/messaging/unsend_message/",
+        "data": dataForm,
+        "cookies": parse_cookie_string(dataFB["cookieFacebook"]),
+        "verify": True
+    }
+     
+     sendRequests = json.loads(requests.post(**mainRequests).text.split("for (;;);")[1])
+
+     if (sendRequests.get("error")):
+          return Exception({"error": str(sendRequests)})
+     else:
+          return {
+               "success": 1,
+               "messages": "Thu hồi tin nhắn thành công."
+          }
+
+""" Hướng dẫn sử dụng (Tutorial)
+
+ * Dữ liệu yêu cầu (args):
+
+     -setCookies: Cookie account Facebook
+     - dataFB: lấy từ __facebookToolsV2.dataGetHome(setCookies)
+     - messageID: Message ID của tin nhắn (Example: mid.$gABESRz00DD6........)
+
+* Kết quả trả về:
+
+     - Không có dữ liệu
+     
+     - Ghi chú: tùy thuộc vào nhiều trường hợp mà error có thể báo code lỗi và chi tiết khác nhau!
+
+* Thông tin tác giả:
+     Facebook:  m.me/Booking.MinhHuyDev
+     Telegram: t.me/minhhuydev
+     Github: MinhHuyDev
+
+✓Remake by Nguyễn Minh Huy
+✓Remake from Fbchat Python (https://fbchat.readthedocs.io/en/stable/)
+✓Hoàn thành vào lúc 09:36 ngày 30/6/2023 • Cập nhật mới nhất: Không có dữ liệu
+✓Tôn trọng tác giả ❤️
+"""
