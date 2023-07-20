f6a40f6256b15adae4b297aed8acb0e3fd44acc9
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2023-07-20 07:11:09 +0700

    clear code

--- src/__getCommand.py
+++ src/__getCommand.py
@@ -1,65 +1,41 @@
 import json
-def Main(contents):
 
-     try: ContentsArgs = contents.split()
-     except:
-          return json.dumps({
-          "results": {
-               "command": "ERR",
-               "args": {
+def Main(contents):
+    try:
+        ContentsArgs = contents.split()
+    except:
+        return json.dumps({
+            "results": {
+                "command": "ERR",
+                "args": {
                     "1": "ERR",
                     "2": "ERR",
                     "3": "ERR",
                     "4": "ERR",
                     "full": "ERR"
-               }
-          }
-     }, ensure_ascii=False)
+                }
+            }
+        }, ensure_ascii=False)
 
-     if len(ContentsArgs) >= 5:
-         Args1 = ContentsArgs[1]
-         Args2 = ContentsArgs[2]
-         Args3 = ContentsArgs[3]
-         Args4 = " ".join([ContentsArgs[i] for i in range(4, len(ContentsArgs))])
-         ArgsFull = f"{Args1} {Args2} {Args3} {Args4}"
-     elif len(ContentsArgs) == 4:
-         Args1 = ContentsArgs[1]
-         Args2 = ContentsArgs[2]
-         Args3 = ContentsArgs[3]
-         Args4 = None
-         ArgsFull = f"{Args1} {Args2} {Args3}"
-     elif len(ContentsArgs) == 3:
-         Args1 = ContentsArgs[1]
-         Args2 = ContentsArgs[2]
-         Args3 = None
-         Args4 = ""
-         ArgsFull = f"{Args1} {Args2}"
-     elif len(ContentsArgs) == 2:
-         Args1 = ContentsArgs[1]
-         Args2 = None
-         Args3 = None
-         Args4 = None
-         ArgsFull = Args1
-     else:
-         Args1 = None
-         Args2 = None
-         Args3 = None
-         Args4 = None
-         ArgsFull = None
-     
-     return json.dumps({
-          "results": {
-               "command": ContentsArgs[0],
-               "args": {
-                    "1": Args1,
-                    "2": Args2,
-                    "3": Args3,
-                    "4": Args4,
-                    "full": ArgsFull
-               }
-          }
-     }, ensure_ascii=False)
+    num_args = len(ContentsArgs) - 1
+    ArgsFull = " ".join(ContentsArgs[1:])
+    Args1 = ContentsArgs[1] if num_args >= 1 else None
+    Args2 = ContentsArgs[2] if num_args >= 2 else None
+    Args3 = ContentsArgs[3] if num_args >= 3 else None
+    Args4 = " ".join([ContentsArgs[i] for i in range(4, len(ContentsArgs))]) if num_args >= 4 else None
 
-     
-print(Main(",help 1 2 3 4 huy dz"))
+    return json.dumps({
+        "results": {
+            "command": ContentsArgs[0],
+            "args": {
+                "1": Args1,
+                "2": Args2,
+                "3": Args3,
+                "4": Args4,
+                "full": ArgsFull
+            }
+        }
+    }, ensure_ascii=False)
 
+
+print(Main(",help 1 2 3"))
