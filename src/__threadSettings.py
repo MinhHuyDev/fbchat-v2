89160de0720ee75ac3c238b7229ffd2cae7db1e3
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2023-07-20 07:57:05 +0700

    Add files via upload

--- src/__threadSettings.py
+++ src/__threadSettings.py
@@ -1,70 +1,11 @@
-import json, requests, time, json, attr, random
-# from LorenBot.plugins import __facebookToolsV2
+import json, requests, time, json, random
 import datetime
 import __facebookToolsV2
+from utils import digitToChar, str_base, parse_cookie_string, Headers, formAll
 
-def digitToChar(digit):
-          if digit < 10:
-               return str(digit)
-          return chr(ord("a") + digit - 10)
-
-def str_base(number, base):
-     if number < 0:
-          return "-" + str_base(-number, base)
-     (d, m) = divmod(number, base)
-     if d > 0:
-          return str_base(d, base) + digitToChar(m)
-     return digitToChar(m)
-
-def parse_cookie_string(cookie_string):
-     cookie_dict = {}
-     cookies = cookie_string.split(";")
-
-     for cookie in cookies:
-          if "=" in cookie:
-               key, value = cookie.split("=")
-          else:
-               pass
-          try: cookie_dict[key] = value
-          except: pass
-
-     return cookie_dict
-
-def Headers(setCookies, dataForm=None):
-     headers = {}
-     headers["Host"] = "www.facebook.com"
-     headers["Connection"] = "keep-alive"
-     if (dataForm != None):
-          headers["Content-Length"] = str(len(dataForm))
-     headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
-     headers["Accept"] = "*/*"
-     headers["Origin"] = "https://www.facebook.com"
-     headers["Sec-Fetch-Site"] = "same-origin"
-     headers["Sec-Fetch-Mode"] = "cors"
-     headers["Sec-Fetch-Dest"] = "empty"
-     headers["Referer"] = "https://www.facebook.com/"
-     headers["Accept-Language"] = "vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7"
-
-
-def formAll(dataFB):
-     __reg = attr.ib(0).counter
-     _revision = attr.ib()
-     __reg += 1 
-     dataForm = {}
-     
-     dataForm["fb_dtsg"] = dataFB["fb_dtsg"]
-     dataForm["jazoest"] = dataFB["jazoest"]
-     dataForm["__a"] = 1
-     dataForm["__user"] =str(dataFB["FacebookID"])
-     dataForm["__req"] = str_base(__reg, 36) 
-     dataForm["__rev"] = dataFB["client_revision"]
-     dataForm["av"] = dataFB["FacebookID"]
-
-     return dataForm
 def addUserToAdminThread(threadID, idUser, statusChoice, dataFB): # Thêm admin mới trong nhóm
     
-    dataForm = formAll(dataFB)
-
+    dataForm = formAll(dataFB, requireGraphql=False)
     dataForm["thread_fbid"] = str(threadID)
     dataForm["admin_ids[0]"] = str(idUser)
     dataForm["add"] = statusChoice
@@ -82,11 +23,11 @@ def addUserToAdminThread(threadID, idUser, statusChoice, dataFB): # Thêm admin 
     if sendRequests.get("error"):
         error = sendRequests["error"]
         if error == 1976004:
-              return Exception({"error": "Không thể thay đổi trạng thái quản trị viên: bạn không phải là quản trị viên."})
+              return {"error": "Bạn không phải là quản trị viên."}
         elif error == 1357031:
-              return Exception({"error": "Không thể thay đổi trạng thái quản trị viên: chủ đề này không phải là một cuộc trò chuyện nhóm."})
+              return {"error": "Chủ đề này không phải là một cuộc trò chuyện nhóm."}
         else:
-              return Exception({"error": "Không thể thay đổi trạng thái quản trị viên: lỗi không xác định."})
+              return {"error": "Lỗi không xác định."}
     else:
          return {
               "success": 1,
@@ -95,8 +36,7 @@ def addUserToAdminThread(threadID, idUser, statusChoice, dataFB): # Thêm admin 
 
 def changeNicknameUser(threadID, idUser , NewNickname, dataFB): # Thay đổi biệt danh người dùng
 
-     dataForm = formAll(dataFB)
-
+     dataForm = formAll(dataFB, requireGraphql=False)
      dataForm["nickname"] = NewNickname
      dataForm["participant_id"] = idUser
      dataForm["thread_or_other_fbid"] = threadID
@@ -115,11 +55,11 @@ def changeNicknameUser(threadID, idUser , NewNickname, dataFB): # Thay đổi bi
      if sendRequests.get("error"):
           error = sendRequests.get("error")
           if error == 1545014:
-               return Exception({"error": "Đã xảy ra lỗi: Người dùng không tồn tại trong nhóm/cuộc trò chuyện."})
+               return {"error": "Người dùng không tồn tại trong nhóm/cuộc trò chuyện."}
           elif error == 1357031:
-               return Exception({"error": "Đã xảy ra lỗi: Người dùng không tồn tại."})
+               return {"error": "Người dùng không tồn tại."}
           else:
-               return Exception({"error": "Lỗi không xác định"})
+               return {"error": "Lỗi không xác định"}
      else:
           return {
                "success": 1,
@@ -128,8 +68,7 @@ def changeNicknameUser(threadID, idUser , NewNickname, dataFB): # Thay đổi bi
      
 def changeThreadEmoji(threadID, newEmoji, dataFB): # Thay đổi biểu tượng cảm xúc nhanh 
 
-     dataForm = formAll(dataFB)
-
+     dataForm = formAll(dataFB, requireGraphql=False)
      dataForm["emoji_choice"] = newEmoji
      dataForm["thread_or_other_fbid"] = threadID
      
@@ -147,9 +86,9 @@ def changeThreadEmoji(threadID, newEmoji, dataFB): # Thay đổi biểu tượng
      if (sendRequests.get("error")):
           error = sendRequests.get("error")
           if error == 1357031:
-               return Exception({"error": "Không thể thay đổi trạng thái emoji của một cuộc trò chuyện không tồn tại."})
+               return {"error": "Không thể thay đổi trạng thái emoji của một cuộc trò chuyện không tồn tại."}
           else:
-               return Exception({"error": "Lỗi không xác định"})
+               return {"error": "Lỗi không xác định"}
      else:
           return {
                "success": 1,
@@ -160,8 +99,7 @@ def changeNameThread(threadID, newNameThread, dataFB): # Thay đổi tên nhóm
      
      randomNumber = str(int(format(int(time.time() * 1000), "b") + ("0000000000000000000000" + format(int(random.random() * 4294967295), "b"))[-22:], 2))
 
-     dataForm = formAll(dataFB)
-
+     dataForm = formAll(dataFB, requireGraphql=False)
      dataForm["client"] = "mercury"
      dataForm["action_type"] = "ma-type:log-message"
      dataForm["thread_id"] = ""
@@ -209,9 +147,9 @@ def changeNameThread(threadID, newNameThread, dataFB): # Thay đổi tên nhóm
      if (sendRequests.get("error")):
           error = sendRequests.get("error")
           if error == 1545012:
-               return Exception({"error": "Bạn không thể thay đổi tên nhóm khi bạn không phải là một thành viên của nhóm"})
+               return {"error": "Bạn không thể thay đổi tên nhóm khi bạn không phải là một thành viên của nhóm"}
           elif error == 1545003:
-               return Exception({"error": "Không thể thay đổi tên nhóm không tồn tại."})  
+               return {"error": "Không thể thay đổi tên nhóm không tồn tại."}
      else:
           return {
                "success": 1,
@@ -255,6 +193,6 @@ def changeNameThread(threadID, newNameThread, dataFB): # Thay đổi tên nhóm
 
 ✓Remake by Nguyễn Minh Huy
 ✓Remake from Fbchat Python (https://fbchat.readthedocs.io/en/stable/)
-✓Hoàn thành vào lúc 03:32 ngày 28/6/2023 • Cập nhật mới nhất: Không có dữ liệu: 13:43 28/06/2023
+✓Hoàn thành vào lúc 03:32 ngày 28/6/2023 • Cập nhật mới nhất: Không có dữ liệu: 7:56 20/7/2023
 ✓Tôn trọng tác giả ❤️
 """
