aebb1bf9840b16568c7512e143f5622e1dcd1bd1
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2023-08-05 07:57:37 +0700

    Add files via upload

--- src/__facebookLoginV2.py
+++ src/__facebookLoginV2.py
@@ -1,191 +1,179 @@
 import requests
 import re
-import hashlib
 import string
 import random
 import json
-import html
-from bs4 import BeautifulSoup as BS
 
 """
 Written by Nguyen Minh Huy (RainTee)
 Facebook Login V2 - Fixed
-Updated: 28/12/2022
+Dstetime: 28/12/2022
+Last Update: 04/08/2023
 """
 
+def jsonResults(dataJson, statusLogin, listExportCookies=None):
+     if (statusLogin == 1):
+          return {
+               "success": {
+                    "setCookies": "".join(listExportCookies),
+                    "accessTokenFB": dataJson["access_token"],
+                    "cookiesKey-ValueList": dataJson["session_cookies"]
+               }
+          }
+     else:
+          return {
+               "error": {
+                    "title": dataJson["error"]["error_user_title"],
+                    "description": dataJson["error"]["error_user_msg"],
+                    "error_subcode": dataJson["error"]["error_subcode"],
+                    "error_code": dataJson["error"]["code"],
+                    "fbtrace_id": dataJson["error"]["fbtrace_id"],
+               }
+          }
+               
 def randStr(length):
     return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
-
+         
 def GetToken2FA(key2Fa):
-    twoFARequests = json.loads(requests.get("https://2fa.live/tok/" + key2Fa.replace(" ","")).text)
-    return twoFARequests["token"]
+     try:
+          twoFARequests = json.loads(requests.get("https://2fa.live/tok/" + key2Fa.replace(" ","")).text)
+          return twoFARequests["token"] 
+     except:
+          return random.randint(100000, 999999) # TROLL TIME (appears only when it has an error) ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯
+
+class loginFB:
 
 
-def Headers(cookiesFB=None):
-     headers={}
-     headers["Host"] = "mbasic.facebook.com";
-     headers["origin"] = "https://mbasic.facebook.com";
-     headers["content-type"] = "application/x-www-form-urlencoded";
-     headers["user-agent"] = "Mozilla/5.0 (Linux; Android 11; M2101K7BG Build/RP1A.200720.011;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.4896.79 Mobile Safari/537.36";
-     headers["accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
-     headers["referer"] = "https://mbasic.facebook.com/login/?";
-     headers["accept-language"] = "en-US,en;q=0.9";
-     headers["x-requested-with"] = "mark.via.gp";
-     headers["cookie"] = cookiesFB;
-     return headers
-def loginFacebook(userFB=None, passFB=None, authenticationGG=None):
-     SessionRQ = requests.Session()
-     
-     headers = {
-          "Host": "b-graph.facebook.com",
-          "Content-Type": "application/x-www-form-urlencoded",
-          "X-Fb-Connection-Type": "unknown",
-          "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G988N Build/NRD90M) [FBAN/FB4A;FBAV/340.0.0.27.113;FBPN/com.facebook.katana;FBLC/vi_VN;FBBV/324485361;FBCR/Viettel Mobile;FBMF/samsung;FBBD/samsung;FBDV/SM-G988N;FBSV/7.1.2;FBCA/x86:armeabi-v7a;FBDM/{density=1.0,width=540,height=960};FB_FW/1;FBRV/0;]",
-          "X-Fb-Connection-Quality": "EXCELLENT",
-          "Authorization": "OAuth null",
-          "X-Fb-Friendly-Name": "authenticate",
-          "Accept-Encoding": "gzip, deflate", 
-          "X-Fb-Server-Cluster": "True",
-     }
-     
-     device_id = randStr(8) +"-"+randStr(4) +"-"+ randStr(4)+"-"+ randStr(4) +"-"+ randStr(12)
-     adid = randStr(8)+"-" +randStr(4)+"-" + randStr(4)+"-"+ randStr(4)+"-" + randStr(12)
-     secure_family_device_id =randStr(8)+"-" +randStr(4)+"-" + randStr(4)+"-"+ randStr(4)+"-" + randStr(12)
-     machine_id = randStr(24)
-     
-     dataForm = {
-          "adid": adid,
-          "format": "json",
-          "device_id": device_id,
-          "email": userFB,
-          "password": passFB,
-          "generate_analytics_claim": "1",
-          "community_id": "",
-          "cpl": "true",
-          "try_num": "1",
-          "family_device_id": device_id,
-          "secure_family_device_id":  secure_family_device_id,
-          "credentials_type": "password",
-          "fb4a_shared_phone_cpl_experiment": "fb4a_shared_phone_nonce_cpl_at_risk_v3",
-          "fb4a_shared_phone_cpl_group": "enable_v3_at_risk",
-          "enroll_misauth": "false",
-          "generate_session_cookies": "1",
-          "error_detail_type": "button_with_disabled",
-          "source": "login",
-          "machine_id": machine_id ,
-          "jazoest": "22421",
-          "meta_inf_fbmeta": "",
-          "advertiser_id": adid,
-          "encrypted_msisdn": "",
-          "currently_logged_in_userid": "0",
-          "locale": "vi_VN",
-          "client_country_code": "VN",
-          "fb_api_req_friendly_name": "authenticate",
-          "fb_api_caller_class": "Fb4aAuthHandler",
-          "api_key": "882a8490361da98702bf97a021ddc14d",
-          "access_token": "350685531728|62f8ce9f74b12f84c123cc23437a4a32"
-     }
-    
-     dataJson = json.loads(requests.post("https://b-graph.facebook.com/auth/login",data=dataForm, headers=headers).text)
-     if (dataJson.get("error") != None):
-          if (dataJson["error"]["error_subcode"] == 1348162):
-               factor = dataJson["error"]["error_data"]["login_first_factor"]
-               uidFB = dataJson["error"]["error_data"]["uid"]
-               Get2FA = GetToken2FA(authenticationGG)
-               dataForm2Fa = {
-                    "adid": adid,
-                    "format": "json",
-                    "device_id": device_id,
-                    "email": userFB,
-                    "password": Get2FA,
-                    "generate_analytics_claim": "1",
-                    "community_id": "",
-                    "cpl": "true",
-                    "try_num": "2",
-                    "family_device_id": device_id,
-                    "secure_family_device_id": secure_family_device_id,
-                    "sim_serials": "[]",
-                    "credentials_type": "two_factor",     
-                    "fb4a_shared_phone_cpl_experiment": "fb4a_shared_phone_nonce_cpl_at_risk_v3",
-                    "fb4a_shared_phone_cpl_group": "enable_v3_at_risk",
-                    "enroll_misauth": "false",
-                    "generate_session_cookies": "1",
-                    "error_detail_type": "button_with_disabled",
-                    "source": "login",
-                    "machine_id": machine_id,
-                    "jazoest": "22327",
-                    "meta_inf_fbmeta": "",
-                    "twofactor_code": Get2FA,
-                    "userid": uidFB,
-                    "first_factor": factor,
-                    "advertiser_id": adid,
-                    "encrypted_msisdn": "",
-                    "currently_logged_in_userid": "0",
-                    "locale": "vi_VN",
-                    "client_country_code": "VN",
-                    "fb_api_req_friendly_name": "authenticate",
-                    "fb_api_caller_class": "Fb4aAuthHandler",
-                    "api_key": "882a8490361da98702bf97a021ddc14d",                    
-                    "access_token": "350685531728|62f8ce9f74b12f84c123cc23437a4a32"
-               }
-               pass2Fa = json.loads(requests.post("https://b-graph.facebook.com/auth/login",data=dataForm2Fa, headers=headers).text)
-               if (pass2Fa.get("error") == None):
-                    try:
-                         listExportCookies=[]
-                         for totalCount in range(len(pass2Fa["session_cookies"])):
-                              try:
-                                   listExportCookies.append(pass2Fa["session_cookies"][totalCount + 1 - 1]["name"] + "=" + pass2Fa["session_cookies"][totalCount + 1 - 1]["value"] + "; ")
-                              except:
-                                   break
-                         return {
-                              "success": {
-                                   "setCookies": "".join(listExportCookies),
-                                   "accessTokenFB": pass2Fa["access_token"],
-                                   "cookiesKey-ValueList": pass2Fa["session_cookies"]
-                              }
-                         }
-                    except:
-                         return {
-                              "error": {}
-                         }
+     def __init__(self, username, password, AuthenticationGoogleCode=None):
+          
+          self.deviceID = self.adID = self.secureFamilyDeviceID = f"{randStr(8)}-{randStr(4)}-{randStr(4)}-{randStr(4)}-{randStr(12)}"
+          self.manchineID = randStr(24)
+          self.usernameFacebook = username # IDFB or email/phone number need login (IDFB hoặc email/sđt cần đăng nhập)
+          self.passwordFacebook = password # Password of the account (Mật khẩu của tài khoản)
+          self.twoTokenAccess = AuthenticationGoogleCode # string of 16 characters (or more) provided by Facebook (một chuỗi gồm 16 kí tụ (hoặc hơn) được cấp bởi Facebook)
+          
+          """
+          Note: 
+               - English: If you don't have two-factor authentication set up, you can skip it.
+               - Vietnamese: Nếu bạn không thiết lập xác thực hai yếu tố, bạn có thể bỏ qua nó.
+          """
+          
+     def main(self):
+          
+          headers = {}
+          headers["Host"] = "b-graph.facebook.com"
+          headers["Content-Type"] = "application/x-www-form-urlencoded"
+          headers["X-Fb-Connection-Type"] = "unknown"
+          headers["User-Agent"] = "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G988N Build/NRD90M) [FBAN/FB4A;FBAV/340.0.0.27.113;FBPN/com.facebook.katana;FBLC/vi_VN;FBBV/324485361;FBCR/Viettel Mobile;FBMF/samsung;FBBD/samsung;FBDV/SM-G988N;FBSV/7.1.2;FBCA/x86:armeabi-v7a;FBDM/{density=1.0,width=540,height=960};FB_FW/1;FBRV/0;]"
+          headers["X-Fb-Connection-Quality"] = "EXCELLENT"
+          headers["Authorization"] = "OAuth null"
+          headers["X-Fb-Friendly-Name"] = "authenticate"
+          headers["Accept-Encoding"] = "gzip, deflate"
+          headers["X-Fb-Server-Cluster"] = "True"
+
+          
+          dataForm = {}
+          dataForm["adid"] = self.adID
+          dataForm["format"] = "json"
+          dataForm["device_id"] = self.deviceID
+          dataForm["email"] = self.usernameFacebook
+          dataForm["password"] = self.passwordFacebook
+          dataForm["generate_analytics_claim"] = "1"
+          dataForm["community_id"] = ""
+          dataForm["cpl"] = "true"
+          dataForm["try_num"] = "1"
+          dataForm["family_device_id"] = self.deviceID
+          dataForm["secure_family_device_id"] = self.secureFamilyDeviceID
+          dataForm["credentials_type"] = "password"
+          dataForm["fb4a_shared_phone_cpl_experiment"] = "fb4a_shared_phone_nonce_cpl_at_risk_v3"
+          dataForm["fb4a_shared_phone_cpl_group"] = "enable_v3_at_risk"
+          dataForm["enroll_misauth"] = "false"
+          dataForm["generate_session_cookies"] = "1"
+          dataForm["error_detail_type"] = "button_with_disabled"
+          dataForm["source"] = "login"
+          dataForm["machine_id"] = self.manchineID
+          dataForm["jazoest"] = "22421"
+          dataForm["meta_inf_fbmeta"] = ""
+          dataForm["advertiser_id"] = self.adID
+          dataForm["encrypted_msisdn"] = ""
+          dataForm["currently_logged_in_userid"] = "0"
+          dataForm["locale"] = "vi_VN"
+          dataForm["client_country_code"] = "VN"
+          dataForm["fb_api_req_friendly_name"] = "authenticate"
+          dataForm["fb_api_caller_class"] = "Fb4aAuthHandler"
+          dataForm["api_key"] = "882a8490361da98702bf97a021ddc14d"
+          dataForm["access_token"] = "350685531728|62f8ce9f74b12f84c123cc23437a4a32"
+
+         
+          dataJson = json.loads(requests.post("https://b-graph.facebook.com/auth/login",data=dataForm, headers=headers).text)
+          if (dataJson.get("error") != None):
+               if (dataJson["error"]["error_subcode"] == 1348162):
+                    Get2FA = GetToken2FA(self.twoTokenAccess)
+                    dataForm2Fa = {}
+                    dataForm2Fa["adid"] = self.adID
+                    dataForm2Fa["format"] = "json"
+                    dataForm2Fa["device_id"] = self.deviceID
+                    dataForm2Fa["email"] = self.usernameFacebook
+                    dataForm2Fa["password"] = Get2FA
+                    dataForm2Fa["generate_analytics_claim"] = "1"
+                    dataForm2Fa["community_id"] = ""
+                    dataForm2Fa["cpl"] = "true"
+                    dataForm2Fa["try_num"] = "2"
+                    dataForm2Fa["family_device_id"] = self.deviceID
+                    dataForm2Fa["secure_family_device_id"] = self.secureFamilyDeviceID
+                    dataForm2Fa["sim_serials"] = "[]"
+                    dataForm2Fa["credentials_type"] = "two_factor"
+                    dataForm2Fa["fb4a_shared_phone_cpl_experiment"] = "fb4a_shared_phone_nonce_cpl_at_risk_v3"
+                    dataForm2Fa["fb4a_shared_phone_cpl_group"] = "enable_v3_at_risk"
+                    dataForm2Fa["enroll_misauth"] = "false"
+                    dataForm2Fa["generate_session_cookies"] = "1"
+                    dataForm2Fa["error_detail_type"] = "button_with_disabled"
+                    dataForm2Fa["source"] = "login"
+                    dataForm2Fa["machine_id"] = self.manchineID
+                    dataForm2Fa["jazoest"] = "22327"
+                    dataForm2Fa["meta_inf_fbmeta"] = ""
+                    dataForm2Fa["twofactor_code"] = Get2FA
+                    dataForm2Fa["userid"] = dataJson["error"]["error_data"]["uid"]
+                    dataForm2Fa["first_factor"] = dataJson["error"]["error_data"]["login_first_factor"]
+                    dataForm2Fa["advertiser_id"] = self.adID
+                    dataForm2Fa["encrypted_msisdn"] = ""
+                    dataForm2Fa["currently_logged_in_userid"] = "0"
+                    dataForm2Fa["locale"] = "vi_VN"
+                    dataForm2Fa["client_country_code"] = "VN"
+                    dataForm2Fa["fb_api_req_friendly_name"] = "authenticate"
+                    dataForm2Fa["fb_api_caller_class"] = "Fb4aAuthHandler"
+                    dataForm2Fa["api_key"] = "882a8490361da98702bf97a021ddc14d"
+                    dataForm2Fa["access_token"] = "350685531728|62f8ce9f74b12f84c123cc23437a4a32"
+                    pass2Fa = json.loads(requests.post("https://b-graph.facebook.com/auth/login",data=dataForm2Fa, headers=headers).text)
+                    if (pass2Fa.get("error") == None):
+                         try:
+                              listExportCookies = []
+                              for cookie in pass2Fa.get("session_cookies", []):
+                                   try:
+                                        listExportCookies.append(f"{cookie['name']}={cookie['value']}; ")
+                                   except KeyError:
+                                        break
+
+                              return dataJson(pass2Fa, 1, listExportCookies)
+                         except Exception as errLog:
+                              return {"error": {"description": str(errLog)}}
+                    else:
+                         return jsonResults(pass2Fa, 0)
                else:
-                    return {
-                         "error": {
-                              "title": pass2Fa["error"]["error_user_title"],
-                              "description": pass2Fa["error"]["error_user_msg"],
-                              "error_subcode": pass2Fa["error"]["error_subcode"],
-                              "error_code": pass2Fa["error"]["code"],
-                              "fbtrace_id": pass2Fa["error"]["fbtrace_id"],
-                         }
-                    }
+                    return jsonResults(dataJson, 0)
           else:
-               return {
-                         "error": {
-                              "title": dataJson["error"]["error_user_title"],
-                              "description": dataJson["error"]["error_user_msg"],
-                              "error_subcode": dataJson["error"]["error_subcode"],
-                              "error_code": dataJson["error"]["code"],
-                              "fbtrace_id": dataJson["error"]["fbtrace_id"],
-                         }
-                    }
-     else:
-          try:
-               listExportCookies=[]
-               for totalCount in range(len(dataJson["session_cookies"])):
-                    try:
-                         listExportCookies.append(dataJson["session_cookies"][totalCount + 1 - 1]["name"] + "=" + dataJson["session_cookies"][totalCount + 1 - 1]["value"] + "; ")
-                    except:
-                         break
-               return {
-                    "success": {
-                         "setCookies": "".join(listExportCookies),
-                         "accessTokenFB": dataJson["access_token"],
-                         "cookiesKey-ValueList": dataJson["session_cookies"]
-                    }
-               }
-          finally:
-               pass# return dataJson
-               
+               try:
+                    listExportCookies=[]
+                    for totalCount in range(len(dataJson["session_cookies"])):
+                         try:
+                              listExportCookies.append(dataJson["session_cookies"][totalCount + 1 - 1]["name"] + "=" + dataJson["session_cookies"][totalCount + 1 - 1]["value"] + "; ")
+                         except:
+                              break
+                    return jsonResults(dataJson, 1, listExportCookies)
+               finally:
+                    pass# return dataJson
+
+
 """ Hướng dẫn sử dụng (Tutorial)
 
  * Dữ liệu yêu cầu (args):
@@ -193,6 +181,11 @@ def loginFacebook(userFB=None, passFB=None, authenticationGG=None):
      - userFB: username/phone/gmail Facebook
      - passFB: Mật khẩu Facebook
      - authenticationGG: mã 2fa (nếu có)
+
+* Code mẫu:
+     
+     loginFacebook = loginFB("<userFB>", "<passFB>", "<authenticationGG>")
+     print(loginFacebook.main())
      
 * Kết quả trả về:
      
@@ -206,7 +199,6 @@ def loginFacebook(userFB=None, passFB=None, authenticationGG=None):
      Github: MinhHuyDev
 
 ✓Remake by Nguyễn Minh Huy
-✓Remake from Fbchat Python (https://fbchat.readthedocs.io/en/stable/)
-✓Sửa đổi vào thứ vào lúc 02:39 ngày 26/6/2023 • Cập nhật mới nhất: 02:39 26/06/2023
+✓Sửa đổi mới nhất vào thứ vào lúc 7:56 05/08/2023
 ✓Tôn trọng tác giả ❤️
 """
