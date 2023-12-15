a5735e3ba45abdbee48ba6d6a1bb28fa63c51c92
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2023-12-15 23:32:48 +0700

    Fix a bug

--- src/__unsendMessage.py
+++ src/__unsendMessage.py
@@ -1,13 +1,12 @@
 import requests, json
-import __facebookToolsV2 
-from utils import digitToChar, str_base, parse_cookie_string, Headers, formAll
+from utils import digitToChar, str_base, parse_cookie_string, Headers, formAll, mainRequests
  
 def _unsend(messageID, dataFB):
 
      dataForm = formAll(dataFB, requireGraphql=False)
      dataForm["message_id"] = messageID
 
-     sendRequests = json.loads(requests.post(**mainRequests("https://www.facebook.com/messaging/unsend_message/", dataForm, self.dataFB["cookieFacebook"])).text.split("for (;;);")[1])
+     sendRequests = json.loads(requests.post(**mainRequests("https://www.facebook.com/messaging/unsend_message/", dataForm, dataFB["cookieFacebook"])).text.split("for (;;);")[1])
 
      if (sendRequests.get("error")):
           return Exception({"error": str(sendRequests)})
