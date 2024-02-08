14580f7dbe3c925cf063c3962f64473d40f25286
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2024-02-08 15:40:46 +0700

    Remove unnecessary libraries

--- src/__getUserInformation.py
+++ src/__getUserInformation.py
@@ -1,7 +1,6 @@
-import json, requests, time, json, attr, random
-import datetime
-import __facebookToolsV2 
-from utils import digitToChar, str_base, parse_cookie_string, Headers, formAll
+import json, requests, json
+# import __facebookToolsV2 
+from utils import parse_cookie_string, Headers, formAll
 
 def Main(dataFB, userID):
      
