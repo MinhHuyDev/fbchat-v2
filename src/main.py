924b6393972be946d1f57cfb9bb18a88c359df2f
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2024-06-09 00:34:27 +0700

    update features

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from __facebookToolsV2 import dataGetHome, fbTools
 from __messageListenV2 import listeningEvent  # Import the specific class or module you need
 from __sendMessage import api
+from __uploadAttachments import _uploadAttachment
 import datetime, threading, os, json
  
 class fbClient:
@@ -10,10 +11,13 @@ class fbClient:
         self.messageID = None
         self.prefix = "/" # This is the command prompt; when you enter this symbol, the corresponding command will be invoked. Additionally, you can customize it as per your preference (e.g., , . * ! ? etc)
         self.pathFile = ".mqttMessage"
+        self.typeAttachment = None
+        self.attachmentID = None
+        self.typeChat = None # Change this value if you want to send the message to a user instead of a group. If you want to send it to a user, replace it with the value: 'user'
         self.recentReceivedMessages = []
 
     def setDefaultValue(self):
-        self.userID, self.bodyMessage, self.replyToID, self.bodySend, self.commandPlugins = [None] * 5
+        self.userID, self.bodyMessage, self.replyToID, self.bodySend, self.commandPlugins, self.typeChat, self.attachmentID, self.typeAttachment= [None] * 8
 
     def receiveCommandAndSend(self):
         if (self.dataFB["FacebookID"] != self.userID):
@@ -24,15 +28,26 @@ class fbClient:
                      self.bodySend = "Hey,", self.userID
                  case "ping":
                      self.bodySend = "Pong!"
+                 case "img":
+                     nameAttachment = "NAME PATH IMAGE" # Change the image path here (only image!)
+                     uploadAttachment = _uploadAttachment(nameAttachment, self.dataFB) # args=("<nameFile>, dataFB)
+                     self.attachmentID = uploadAttachment.get('attachmentID')
+                     self.bodySend = 'Your image!'
+                     self.typeAttachment = "image" # Change this value to match the attachment.
+                     """ WARNING: 
+                         This is just an example for sending images. If you want to send other types of attachments,
+                         you need to change the value of the variable: typeAttachment.
+                         Learn more at: https://github.com/MinhHuyDev/fbchat-v2/blob/main/DOCS.md#uploadAttachmentAndSend
+                     """
                  case __:
                      self.bodySend = self.bodyMessage
              mainSend = api()  # Use the specific class or module you imported
-             threading.Thread(target=mainSend.send, args=(self.dataFB, self.bodySend, self.replyToID)).start()
+             threading.Thread(target=mainSend.send, args=(self.dataFB, self.bodySend, self.replyToID, self.typeAttachment, self.attachmentID, self.typeChat)).start()
              self.setDefaultValue()
 
     def prefixCheck(self):
         if self.bodyMessage[0] == self.prefix:
-            self.commandPlugins = self.bodyMessage.split(',')[1]
+            self.commandPlugins = self.bodyMessage.split(self.prefix)[1]
         else:
             self.commandPlugins = self.bodyMessage
           
@@ -59,15 +74,38 @@ class fbClient:
                        self.bodyMessage = self.bodyMain['body']
                        self.replyToID = self.bodyMain['replyToID']
                        print(f"> userID: {self.userID}\n> messageID: {self.messageID}\n> messageContents: {self.bodyMessage}\n> From {self.bodyMain['type']}ID: {self.replyToID}\n- - - - -")
+                       if (self.bodyMain['type'] != 'thread'): self.typeChat = 'user'
                        self.prefixCheck()
                        self.receiveCommandAndSend()
                        self.setDefaultValue()
-               except:
+               except: # If nothing happens, please replace 'except' with 'finally' to check for errors. 
                    pass
+                   
 
-cookies = "this is set Cookie Facebook"
+cookies = "<cookie-facebook>"
 dataFB = dataGetHome(cookies)
+print(dataFB)
 _ = fbClient(cookies, dataFB)
 _.setDefaultValue()
 _.receiveMessage()
 print("done!")
+
+""" > A Message from Our Hearts to Our Users < 
+
+Dear Users,
+
+I'm Huy, the owner of the fbchat-v2 project. I'm truly grateful for your time using/reading my project. 
+Although it was completed in a short time, I've refined it through numerous tests and 
+bug fixes based on user reports, which I greatly appreciate.
+Therefore, if you encounter any errors in the project, please report them in the issue section of this repository!
+I'd be very thankful for that. Let's be smart and conscious users. Love you all <3
+
+- A note about this source code (main.py): 
+It's just a template/sample code to demonstrate:
+  + Receiving messages
+  + Sending messages
+  + Receiving commands from users
+  + Sending images
+Besides the above, fbchat-v2 has many other powerful features. Please refer to the instructions in DOCS.md to learn how to use them.
+
+"""
