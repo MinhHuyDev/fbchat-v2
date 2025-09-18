ab8f0c87bab48f0a6418634b93e120c8c666ecf4
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2025-09-18 10:32:02 +0700

    fix dataGetHome: Facebook page error!

--- src/__facebookToolsV2.py
+++ src/__facebookToolsV2.py
@@ -1,23 +1,46 @@
 import requests, json, time, random
 from utils import parse_cookie_string, dataSplit, formAll, mainRequests
   
+
 def dataGetHome(setCookies):
      
      mainRequests = {
-               "headers": {
-                    "authority": "m.facebook.com",
-                    "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
-               },
-               "timeout": 60000,
-               "url": "https://www.facebook.com/",
-               "cookies": parse_cookie_string(setCookies),
-               "verify": True
+          "headers": {
+               "authority": "www.facebook.com",
+               "method": "GET",
+               "path": "/",
+               "scheme": "https",
+               "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+               "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+               "cache-control": "max-age=0",
+               "cookie": setCookies,
+               "dpr": "1.25",
+               "priority": "u=0, i",
+               "sec-ch-prefers-color-scheme": "dark",
+               "sec-ch-ua": '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+               "sec-ch-ua-full-version-list": '"Chromium";v="140.0.7339.128", "Not=A?Brand";v="24.0.0.0", "Google Chrome";v="140.0.7339.128"',
+               "sec-ch-ua-mobile": "?0",
+               "sec-ch-ua-model": '""',
+               "sec-ch-ua-platform": '"Windows"',
+               "sec-ch-ua-platform-version": '"19.0.0"',
+               "sec-fetch-dest": "document",
+               "sec-fetch-mode": "navigate",
+               "sec-fetch-site": "same-origin",
+               "sec-fetch-user": "?1",
+               "upgrade-insecure-requests": "1",
+               "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+               "viewport-width": "493"
+          },
+          "timeout": 60000,
+          "url": "https://www.facebook.com/",
+          "cookies": parse_cookie_string(setCookies),
+          "verify": True
      }
      
      dictValueSaved = {}
      splitDataList = [
           # FORMAT: nameValue, stringData_1, stringData_2
-          ["fb_dtsg", "[\"DTSGInitData\",[],{\"token\":\"", "\""],
+          ["fb_dtsg", "DTSGInitialData\",[],{\"token\":\"", "\""],
           ["fb_dtsg_ag", "async_get_token\":\"", "\""],
           ["jazoest", "jazoest=", "\""],
           ["hash", "hash\":\"", "\""],
@@ -27,7 +50,6 @@ def dataGetHome(setCookies):
      ]
      
      sendRequests = requests.get(**mainRequests).text
-     
      for i in splitDataList:
           nameValue = i[0]
           try:
